f72caedf37997d307138bebcf8c52d95846efffa
linusschlepp <linusmo.94@googlemail.com>
2023-10-09 18:00:11 +0200

    Finished working on Contact us form, ready for review!

--- web/script/index.js
+++ web/script/index.js
@@ -1,27 +1,29 @@
 function sendMail() {
+   // fetch parameters from input-fields
   const params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
     message: document.getElementById("message").value,
   };
 
+  //Check if all fields were set in form
   if(Object.values(params).some(param => param === '')){
     alert("Fields must not be empty!");
     return;
   }
 
+   //ToDO: Hide parameters
+  const serviceID = "service_recycle";
+  const templateID = "template_98qnybp";
 
-
-  const serviceID = "service_recycling";
-  const templateID = "template_z5qj2i8";
-
+    // send mail
     emailjs.send(serviceID, templateID, params)
     .then(res=>{
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
         console.log(res);
-        alert("Email was sent successfully!")
+        alert("Email was sent successfully, Thank you!")
 
     })
     .catch(err=>alert(err));
